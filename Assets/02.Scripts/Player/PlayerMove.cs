ae358686dafff38aa2fa8240ef917f0f6d36c6d9
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-02 14:04:00 +0900

    fix 간단히 정리해둠

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -2,85 +2,71 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-     // 이 스크립트의 목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고싶다. 
-     
-     // 필요 필드 :
-     public float Speed;
-     
-     public float MinX = -2.4f;
-     public float MaxX = 2.4f;
-     public float MinY = -4.7f;
-     public float MaxY = 0f;
-     
-     
-     // 매 프레임마다 실행된다.
-     // 초당 프레임 실행 횟수는 : 별다른 설정이 없을 경우 가능한 많이 
-   private void Update()
-   {
-
-         
-         
-         // 1. 키보드 입력을 받는다.
-         // if (Input.GetKey(KeyCode.LeftArrow)) 를 넣으면 왼쪽으로 움직인다는 값을 넣는거임
-         // Debug.Log("왼쪽 방향키를 누르는 중");
-         {
-             float h = Input.GetAxisRaw("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~0 ~ 1f 까지 반환
-             float v = Input.GetAxisRaw("Vertical");    // 키보드 위/아래 입력 상태에 따라 -1f ~0 ~ 1f 까지 반환
-             
-             Debug.Log($"h:{h}, v:{v}");
-
-
-             // 2. 키보드 입력에 따라 방향을 구한다.
-             // 게임에는 벡터라는 타입이 있다 벡터는(크기와 방향을 의미)
-             Vector2 direction = new Vector2(h, v); // 왼쪽 방향
-             // Vector2 direction = Vector2.left;  -> 이미 만들어져있음
-
-
-             // 3. 방향과 속력에 따라 이동한다.
-             // 속도 = 방향 * 속력 
-                                                         // 매직 넘버란 : 보는 사람에 따라 의미가 달라질 수 있는
-                                                         // 헷갈리는 숫자
-             Vector2 normalized = (direction * Speed).normalized;
-             // 벡터의 길이를 1로 만들어 주는 것 (즉,방향만 유지된다) 변수명은 의미가 있어야 한다.
-             
-             transform.Translate(direction * Speed * Time.deltaTime);
-             // deltaTime : 이전 프레임으로 부터 지금 프레임까지 시간이 얼마나 지났는지 Ms단위로 반환 
-             // Ms -> 1/1000 초 
-             
-             // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
-             // transform.position += (Vector3)direction * Speed * Time.deltaTime;
-             
-             float posX = transform.position.x;
-             float posY = Mathf.Clamp(transform.position.y, MinY, MaxY);
-
-             if (posX > MaxX)
-             {
-                 posX = MinX;
-             }
-             else if (posX < MinX)
-             {
-                 posX = MaxX;
-             }
-
-             transform.position = new Vector3(posX, posY, transform.position.z);
-             
-             SpeedChange();
-         }
+    // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
+ 
+    // 필요 필드:
+    public float Speed;
+    public float MaxPositionY;
+    public float MinPositionY;
+    public float MaxPositionX;
+    public float MinPositionX;
+    
+    
+    // 매 프레임마다 실행된다.
+    // 초당 프레임 실행 횟수는: 별다른 설정이 없을 경우 가능한 많이
+    private void Update()
+    {
+        Move();
+        
+        SpeedChange();
     }
 
     private void SpeedChange()
     {
-        // E = 속도 증가
+        // 7. Q/E 버튼 입력을 통한 스피드 업/다운
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Speed += 1f;
+            Speed++;
         }
-
-        // Q = 속도 감소
-        if (Input.GetKeyDown(KeyCode.Q))
+        else if (Input.GetKeyDown(KeyCode.Q))
         {
-            Speed = Mathf.Max(0f, Speed - 1f);
+            Speed--;
         }
+    }
+
+    private void Move()
+    {
+        // 1. 키보드 입력을 받는다.
+        float h = Input.GetAxisRaw("Horizontal"); 
+        float v = Input.GetAxisRaw("Vertical");
+        
+        // 2. 키보드 입력에 따라 방향을 구한다.
+        Vector2 normalizedDirection = new Vector2(h, v).normalized;
+
+        // 3. 방향과 속력에 따라 이동한다.
+        Vector2 newPosition = transform.position + (Vector3)normalizedDirection * Speed * Time.deltaTime;
+
+        // 4. 위치 y에 제한이 있다.
+        if (newPosition.y > MaxPositionY)
+        {
+            newPosition.y = MaxPositionY;
+        }
+        else if (newPosition.y < MinPositionY)
+        {
+            newPosition.y = MinPositionY;
+        }
+        
+        // 5. 양 옆 끝으로 가면 반대쪽 방향으로 이동
+        if (newPosition.x > MaxPositionX)
+        {
+            newPosition.x = MinPositionX;
+        }
+        else if (newPosition.x < MinPositionX)
+        {
+            newPosition.x = MaxPositionX;
+        }
+        
+        transform.position = newPosition;
     }
     
 }
