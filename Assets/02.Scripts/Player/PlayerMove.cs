6823e39937fb57d13c763a59013e02591f36c683
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-02 11:02:37 +0900

    feat 플레이어 이동 기능 추가

---
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+     // 이 스크립트의 목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고싶다. 
+     
+     // 필요 필드 :
+     public float Speed;
+     
+     
+     // 매 프레임마다 실행된다.
+     // 초당 프레임 실행 횟수는 : 별다른 설정이 없을 경우 가능한 많이 
+   private void Update()
+    {
+         // 1. 키보드 입력을 받는다.
+         // if (Input.GetKey(KeyCode.LeftArrow)) 를 넣으면 왼쪽으로 움직인다는 값을 넣는거임
+         // Debug.Log("왼쪽 방향키를 누르는 중");
+         {
+             float h = Input.GetAxis("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~0 ~ 1f 까지 반환
+             float v = Input.GetAxis("Vertical");    // 키보드 위/아래 입력 상태에 따라 -1f ~0 ~ 1f 까지 반환
+             
+             Debug.Log($"h:{h}, v:{v}");
+
+
+             // 2. 키보드 입력에 따라 방향을 구한다.
+             // 게임에는 벡터라는 타입이 있다 벡터는(크기와 방향을 의미)
+             Vector2 direction = new Vector2(h, v); // 왼쪽 방향
+             // Vector2 direction = Vector2.left;  -> 이미 만들어져있음
+
+
+             // 3. 방향과 속력에 따라 이동한다.
+             // 속도 = 방향 * 속력 
+                                                         // 매직 넘버란 : 보는 사람에 따라 의미가 달라질 수 있는
+                                                         // 헷갈리는 숫자
+             transform.Translate(direction * Speed * Time.deltaTime);
+             // deltaTime : 이전 프레임으로 부터 지금 프레임까지 시간이 얼마나 지났는지 Ms단위로 반환 
+             // Ms -> 1/1000 초 
+             
+             // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
+             // transform.position += (Vector3)direction * Speed * Time.deltaTime;
+             
+             
+         }
+    }
+}
