502b554dce55cf93b0012398fd1c636be5a4c8f9
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-02 15:00:23 +0900

    feat 총알 양쪽 발사

---
+++ Assets/02.Scripts/Bullet/CapsuleBullet.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CapsuleBullet : MonoBehaviour
+
+{
+   
+    public float Speed;
+    
+    private void Update()
+    {
+        Vector2 direction = Vector2.up;//  new Vector2(1, 0);
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+}
