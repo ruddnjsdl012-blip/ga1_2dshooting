9f50819b04e84defbf65efee5632b153c4dfd85c
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-02 15:10:26 +0900

    feat 총알 쿨타임 적용

---
+++ Assets/02.Scripts/Bullet/BulletCoolTime.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform FirePoint;
+
+    public float CoolTime = 0.5f;
+
+    private float lastFireTime;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time >= lastFireTime + CoolTime)
+            {
+                Instantiate(BulletPrefab, FirePoint.position, Quaternion.identity);
+
+                lastFireTime = Time.time;
+            }
+        }
+    }
+}
