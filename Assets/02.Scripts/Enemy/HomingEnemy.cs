6ea195b733797dd9dc8d73d21de77afb021a9764
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-04 10:42:36 +0900

    refactor: 캐싱기법적용

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -6,12 +6,11 @@ public class HomingEnemy : Enemy
 
     private void Start()
     {
-        
+        _player = GameObject.FindWithTag("Player");
     }
 
     protected override void Move()
     {
-        _player = GameObject.FindWithTag("Player");
         // 1. 방향을 구한다.
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();
