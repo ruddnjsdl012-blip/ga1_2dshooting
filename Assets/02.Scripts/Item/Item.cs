3edc978eb539f94ead70e381fb1f2382d821af2b
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-07 10:55:01 +0900

    feat: 2차 아이템 구현

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -4,4 +4,42 @@ public class Item : MonoBehaviour
 {
     public string Type;
     public float Value;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        Player player = other.GetComponent<Player>();
+        if (player == null)
+        {
+            Debug.LogWarning("플레이어 태그 오브젝트에 플레이어 컴포넌트가 없습니다.");
+            return;
+        }
+
+        switch (Type)
+        {
+            case "heal":
+            {
+                player.TakeDamage((int)(Value * -1));
+                //Debug.Log($"플레이어 체력: {player}");
+                break;
+            }
+
+            case "moveSpeedUp":
+            {
+                player.GetComponent<PlayerMove>().Speed += Value;
+                Debug.Log($"플레이어 이속: {player.GetComponent<PlayerMove>().Speed}");
+                break;
+            }
+
+            case "fireRateUp":
+            {
+                player.GetComponent<PlayerFire>().CoolTime -= Value;
+                Debug.Log($"플레이어 공속: {player.GetComponent<PlayerFire>().CoolTime}");
+                break;
+            }
+        }
+
+        Destroy(gameObject);
+    }
 }
