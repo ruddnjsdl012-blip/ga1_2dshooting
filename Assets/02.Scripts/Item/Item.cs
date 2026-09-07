7a658d7c825b51d43ae79d01d5be25bc39201ebc
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-07 10:51:49 +0900

    feat: 1차 아이템 구현(간단하게)

---
+++ Assets/02.Scripts/Item/Item.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string Type;
+    public float Value;
+}
--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -12,4 +12,42 @@ public class Player : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Item")) return;
+
+        Item item = other.GetComponent<Item>();
+        if (item == null)
+        {
+            Debug.LogWarning("아이템 태그 오브젝트에 아이템 컴포넌트가 없습니다.");
+            return;
+        }
+
+        switch (item.Type)
+        {
+            case "heal":
+            {
+                _health += (int)item.Value;
+                Debug.Log($"플레이어 체력: {_health}");
+                break;
+            }
+
+            case "moveSpeedUp":
+            {
+                GetComponent<PlayerMove>().Speed += item.Value;
+                Debug.Log($"플레이어 이속: {GetComponent<PlayerMove>().Speed}");
+                break;
+            }
+
+            case "fireRateUp":
+            {
+                GetComponent<PlayerFire>().CoolTime -= item.Value;
+                Debug.Log($"플레이어 공속: {GetComponent<PlayerFire>().CoolTime}");
+                break;
+            }
+        }
+
+        Destroy(other.gameObject);
+    }
 }
