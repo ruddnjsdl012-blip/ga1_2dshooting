04a86eb01b1a2f86582166e48c7562595c607c5b
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-10 10:48:24 +0900

    feat: 자동 이동시 가장 가까운 적에게 이동

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
 
 public class PlayerAutoMove : MonoBehaviour
-{   
+{
     [SerializeField] private float _speed;
-    
 
     private void Update()
     {
-     // 1. 타겟을 구한다.
-     GameObject target = GameObject.FindWithTag("Enemy");
-     if (target == null) return;
-     
-     // 2. 방향을 구한다.
-     Vector3 direction = target.transform.position - transform.position;
-     direction.Normalize();
-     direction.y = 0;
-     
-     //3.속도에 맞게 이동한다.
-     transform.position += direction * _speed * Time.deltaTime;
+        // 1. 타겟을 구한다.
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+        if (targets.Length == 0) return;
 
+        GameObject target = targets[0];
+        float minDistance = float.MaxValue;
+
+        // 1-1. 가장 가까운 타겟을 찾는다.
+        foreach (GameObject enemy in targets)
+        {
+            // 거리를 구해서
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distance < minDistance) // 저장된 거리보다 짧다면
+            {
+                // 타겟 변경
+                minDistance = distance;
+                target = enemy;
+            }
+        }
+
+        // 2. 방향을 구한다.
+        Vector3 direction = target.transform.position - transform.position;
+        direction.Normalize();
+        direction.y = 0;
+
+        // 3. 속도에 맞게 이동을한다.
+        transform.position += direction * _speed * Time.deltaTime;
     }
 }
