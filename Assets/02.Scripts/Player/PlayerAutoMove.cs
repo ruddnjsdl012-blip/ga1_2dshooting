e76856f6f67db5ee3743d89a3c25d29640953121
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-10 11:04:31 +0900

    feat: 플레이어 자동 이동 알고리즘 개선
    stoptracking y 보다 작다면 x

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private int _stopTrackingY = 2;
 
     private GameObject _target = null;
 
-
     private void Update()
     {
-        if (_target == null)
+        if (_target == null || _target.transform.position.y < -_stopTrackingY)
         {
             FindNearestTarget();
         }
@@ -22,9 +22,20 @@ public class PlayerAutoMove : MonoBehaviour
         if (_target == null) return;
 
         // 2. 방향을 구한다.
-        Vector3 direction = _target.transform.position - transform.position;
+        Vector3 diff = _target.transform.position - transform.position;
+        Vector3 direction = diff;
+
+        // 적과 나와의 y축 차이가 3보다 크면 앞으로 가고 아니라면 뒤로가게
+        if (diff.y >= 3)
+        {
+            direction.y = 1;
+        }
+        else
+        {
+            direction.y = -1;
+        }
+
         direction.Normalize();
-        direction.y = 0;
 
         // 3. 속도에 맞게 이동을한다.
         transform.position += direction * _speed * Time.deltaTime;
@@ -42,6 +53,11 @@ public class PlayerAutoMove : MonoBehaviour
         // 1-1. 가장 가까운 타겟을 찾는다.
         foreach (GameObject enemy in targets)
         {
+            if (enemy.transform.position.y < -_stopTrackingY)
+            {
+                continue;
+            }
+
             // 거리를 구해서
             float distance = Vector2.Distance(transform.position, enemy.transform.position);
             if (distance < minDistance) // 저장된 거리보다 짧다면
