a724f64d546d438e02ecd29b29721291c5480a16
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-10 10:51:57 +0900

    refactor: 자동 이동시 적 캐싱 & 메서드 추출을 통한 가독성 향상

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -4,13 +4,39 @@ public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private GameObject _target = null;
+
+
     private void Update()
+    {
+        if (_target == null)
+        {
+            FindNearestTarget();
+        }
+
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_target == null) return;
+
+        // 2. 방향을 구한다.
+        Vector3 direction = _target.transform.position - transform.position;
+        direction.Normalize();
+        direction.y = 0;
+
+        // 3. 속도에 맞게 이동을한다.
+        transform.position += direction * _speed * Time.deltaTime;
+    }
+
+    private void FindNearestTarget()
     {
         // 1. 타겟을 구한다.
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
         if (targets.Length == 0) return;
 
-        GameObject target = targets[0];
+        _target = targets[0];
         float minDistance = float.MaxValue;
 
         // 1-1. 가장 가까운 타겟을 찾는다.
@@ -22,16 +48,8 @@ public class PlayerAutoMove : MonoBehaviour
             {
                 // 타겟 변경
                 minDistance = distance;
-                target = enemy;
+                _target = enemy;
             }
         }
-
-        // 2. 방향을 구한다.
-        Vector3 direction = target.transform.position - transform.position;
-        direction.Normalize();
-        direction.y = 0;
-
-        // 3. 속도에 맞게 이동을한다.
-        transform.position += direction * _speed * Time.deltaTime;
     }
 }
