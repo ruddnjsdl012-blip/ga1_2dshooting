6e3f2913ff8aaac065c6ee4815d30374d386bc25
ruddnjsdl012-blip <ruddnjsdl012@gmail.com>
2026-09-11 11:08:29 +0900

    refactor: EnemySpawner 캡슐화
    public -> private로 캡슐화 진행

--- Assets/02.Scripts/Enemy/EnemySpawnData.cs
+++ Assets/02.Scripts/Enemy/EnemySpawnData.cs
@@ -1,10 +1,11 @@
-//데이터 클래스 : 오직 데이터를 저장하고 전달하기 위해 만드는 클래스
-
 using UnityEngine;
 
 [System.Serializable]
 public class EnemySpawnData
 {
-    public GameObject EnemyPrefab;
-    public int Weight;
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private int _weight;
+
+    public GameObject EnemyPrefab => _enemyPrefab;
+    public int Weight => _weight;
 }
